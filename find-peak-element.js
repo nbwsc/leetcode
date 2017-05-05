1981ae31d282913d75a14c09669c5925afae0f22
wangshichao <nbwsc@icloud.com>
2017-05-05 11:36:56 +0800

    0505

--- find-peak-element.js
+++ find-peak-element.js
@@ -15,14 +15,31 @@ Your solution should be in logarithmic complexity.
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(nums) {
+var findPeakElement_silly = function(nums) {
 	var i = 0
 	nums[-1] = nums[nums.length] = -Infinity;
 	for (;
-		(nums[i] < nums[i - 1]) || (nums[i] < nums[i + 1]) && (i <= nums.length - 1); i++) {
-	}
+		(nums[i] < nums[i - 1]) || (nums[i] < nums[i + 1]) && (i <= nums.length - 1); i++) {}
 	return i
 };
 
-// console.log(findPeakElement([1, 2, 3, 1]))
+var findPeakElement = function(nums) {
+	nums[-1] = nums[nums.length] = -Infinity;
+
+	function binary(head, tail) {
+		var cur = ~~((head + tail) / 2);
+		if (nums[cur] > nums[cur - 1] && nums[cur] > nums[cur + 1]) {
+			return cur;
+		} else if (nums[cur] < nums[cur - 1] && nums[cur] > nums[cur + 1]) {
+			return binary(head, cur);
+		} else {
+			return binary(cur, tail)
+		}
+	}
+	return binary(0, nums.length - 1)
+}
+
+
+
+console.log(findPeakElement([1, 2, 3, 1]))
 console.log(findPeakElement([2, 1]))
